ff43df09eb2422ca35adba4acd853c1ea6f35621
Serife Erguner <seriferabia@gmail.com>
2020-06-23 16:50:26 +0200

    RefreshToken model and repository are created

---
+++ my-reddit-backend/src/main/java/com/company/myredditbackend/persistence/model/RefreshToken.java
@@ -0,0 +1,23 @@
+package com.company.myredditbackend.persistence.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.Instant;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class RefreshToken {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String token;
+    private Instant createdDate;
+}
