572fb93055cf8b03c87b69144b7985e6640ff8c9
Serife Erguner <seriferabia@gmail.com>
2020-06-23 16:51:12 +0200

    User model and repository are created

---
+++ my-reddit-backend/src/main/java/com/company/myredditbackend/persistence/model/User.java
@@ -0,0 +1,36 @@
+package com.company.myredditbackend.persistence.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import java.time.Instant;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    @Email
+    @NotEmpty(message = "Email is required")
+    private String email;
+
+    private Instant created;
+    private boolean enabled;
+}
