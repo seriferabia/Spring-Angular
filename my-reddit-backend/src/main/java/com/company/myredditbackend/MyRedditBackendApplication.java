e6ee9eaf97f3aaae5b465cb61d04e96652f1125f
Serife Erguner <seriferabia@gmail.com>
2020-06-23 15:49:38 +0200

    First commit

---
+++ my-reddit-backend/src/main/java/com/company/myredditbackend/MyRedditBackendApplication.java
@@ -0,0 +1,13 @@
+package com.company.myredditbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRedditBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyRedditBackendApplication.class, args);
+    }
+
+}
