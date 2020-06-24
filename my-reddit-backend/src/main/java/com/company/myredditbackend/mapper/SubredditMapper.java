160327e88344447f2f8001258292f9e70bcd16b5
Serife Erguner <seriferabia@gmail.com>
2020-06-24 13:21:06 +0200

    SubredditMapper interface is created using MapStruct

---
+++ my-reddit-backend/src/main/java/com/company/myredditbackend/mapper/SubredditMapper.java
@@ -0,0 +1,25 @@
+package com.company.myredditbackend.mapper;
+
+import com.company.myredditbackend.persistence.dto.SubredditDTO;
+import com.company.myredditbackend.persistence.model.Post;
+import com.company.myredditbackend.persistence.model.Subreddit;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface SubredditMapper {
+
+    @Mapping(target = "numberOfPosts", expression = "java(mapPostsNumber(subreddit.getPosts()))")
+    SubredditDTO mapSubredditToDto(Subreddit subreddit);
+
+    default Integer mapPostsNumber(List<Post> posts) {
+        return posts.size();
+    }
+
+    @InheritInverseConfiguration
+    @Mapping(target = "posts", ignore = true)
+    Subreddit mapDtoToSubreddit(SubredditDTO subredditDTO);
+}
