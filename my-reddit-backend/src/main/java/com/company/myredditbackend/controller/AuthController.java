1cea9ed5f62c94633f87ce447f5a5b9af807944b
Serife Erguner <seriferabia@gmail.com>
2020-06-24 09:15:31 +0200

    Signup functionality is created.

---
+++ my-reddit-backend/src/main/java/com/company/myredditbackend/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.company.myredditbackend.controller;
+
+import com.company.myredditbackend.persistence.dto.RegisterRequest;
+import com.company.myredditbackend.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest){
+        authService.signup(registerRequest);
+        return new ResponseEntity<>("User Registration Successful", HttpStatus.OK);
+    }
+}
