c88dd55c1017a6ad5b78588388023acab58cd1eb
Serife Erguner <seriferabia@gmail.com>
2020-06-24 10:20:29 +0200

    User detail service is implemented.

---
+++ my-reddit-backend/src/main/java/com/company/myredditbackend/service/UserDetailsServiceImpl.java
@@ -0,0 +1,38 @@
+package com.company.myredditbackend.service;
+
+import com.company.myredditbackend.persistence.model.User;
+import com.company.myredditbackend.persistence.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+
+import static java.util.Collections.singletonList;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) {
+        User user = userRepository.findByUsername(username).orElseThrow(() ->
+                new UsernameNotFoundException("User not found with name: " + username));
+
+        return new org.springframework.security.core.userdetails
+                .User(user.getUsername(), user.getPassword(), user.isEnabled(),
+                true, true, true, getAuthorities("USER"));
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(String role) {
+        return singletonList(new SimpleGrantedAuthority(role));
+    }
+}
