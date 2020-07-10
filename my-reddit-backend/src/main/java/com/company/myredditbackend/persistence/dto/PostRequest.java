994cf7541d661159217e5dc40db3a178a1a7f85d
Serife Erguner <seriferabia@gmail.com>
2020-07-10 17:25:18 +0200

    Request and response dto classes are added for post model

---
+++ my-reddit-backend/src/main/java/com/company/myredditbackend/persistence/dto/PostRequest.java
@@ -0,0 +1,18 @@
+package com.company.myredditbackend.persistence.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostRequest {
+    private Long postId;
+    private String subredditName;
+    private String postName;
+    private String url;
+    private String description;
+}
