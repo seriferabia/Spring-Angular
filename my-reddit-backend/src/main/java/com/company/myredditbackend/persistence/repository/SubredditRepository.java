c37f9a8363174f28c8a858bbc1fbb5b242b139f2
Serife Erguner <seriferabia@gmail.com>
2020-07-10 17:28:49 +0200

    New methods are added.

--- my-reddit-backend/src/main/java/com/company/myredditbackend/persistence/repository/SubredditRepository.java
+++ my-reddit-backend/src/main/java/com/company/myredditbackend/persistence/repository/SubredditRepository.java
@@ -3,5 +3,8 @@ package com.company.myredditbackend.persistence.repository;
 import com.company.myredditbackend.persistence.model.Subreddit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SubredditRepository extends JpaRepository<Subreddit, Long> {
+    Optional<Subreddit> findByName(String subredditName);
 }
--- my-reddit-backend/src/main/java/com/company/myredditbackend/persistence/repository/VerificationTokenRepository.java
+++ my-reddit-backend/src/main/java/com/company/myredditbackend/persistence/repository/VerificationTokenRepository.java
@@ -3,5 +3,8 @@ package com.company.myredditbackend.persistence.repository;
 import com.company.myredditbackend.persistence.model.VerificationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+    Optional<VerificationToken> findByToken(String token);
 }
