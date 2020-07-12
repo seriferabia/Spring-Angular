254cc70a650d14eeeff51b4be6a77c5ec3c80ba1
Serife Erguner <seriferabia@gmail.com>
2020-07-12 20:55:31 +0200

    PostMapper is changed as abstract class to be able to add new fields and functions.

--- my-reddit-backend/src/main/java/com/company/myredditbackend/mapper/PostMapper.java
+++ my-reddit-backend/src/main/java/com/company/myredditbackend/mapper/PostMapper.java
@@ -5,25 +5,48 @@ import com.company.myredditbackend.persistence.dto.PostResponse;
 import com.company.myredditbackend.persistence.model.Post;
 import com.company.myredditbackend.persistence.model.Subreddit;
 import com.company.myredditbackend.persistence.model.User;
+import com.company.myredditbackend.persistence.repository.CommentRepository;
+import com.company.myredditbackend.persistence.repository.VoteRepository;
+import com.company.myredditbackend.service.AuthService;
+import com.github.marlonlom.utilities.timeago.TimeAgo;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring")
-public interface PostMapper {
+public abstract class PostMapper {
+    @Autowired
+    CommentRepository commentRepository;
+    @Autowired
+    VoteRepository voteRepository;
+    @Autowired
+    AuthService authService;
+
     @Mappings({
-            @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())"),
-            @Mapping(target = "postRequest.description", source = "description"),
             @Mapping(target = "id", ignore = true),
+            @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())"),
+            @Mapping(target = "description", source = "postRequest.description"),
             @Mapping(target = "subreddit", source = "subreddit"),
             @Mapping(target = "user", source = "user"),
+            @Mapping(target = "voteCounter", constant = "0")
 
     })
-    Post mapRequestToPost(PostRequest postRequest, Subreddit subreddit, User user);
+    public abstract Post mapRequestToPost(PostRequest postRequest, Subreddit subreddit, User user);
 
     @Mappings({
             @Mapping(target = "userName", source = "user.username"),
-            @Mapping(target = "subredditName", source = "subreddit.name")
+            @Mapping(target = "subredditName", source = "subreddit.name"),
+            @Mapping(target = "commentCounter", expression = "java(commentCount(post))"),
+            @Mapping(target = "duration", expression = "java(getDuration(post))")
     })
-    PostResponse mapPostToResponse(Post post);
+    public abstract PostResponse mapPostToResponse(Post post);
+
+    Integer commentCount(Post post) {
+        return commentRepository.findAllByPost(post).size();
+    }
+
+    String getDuration(Post post) {
+        return TimeAgo.using(post.getCreatedDate().toEpochMilli());
+    }
 }
