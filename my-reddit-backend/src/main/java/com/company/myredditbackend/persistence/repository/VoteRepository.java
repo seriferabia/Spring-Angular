9e1cae0a46609d09dddb682d2ec6f70a9595543a
Serife Erguner <seriferabia@gmail.com>
2020-07-12 20:57:01 +0200

    A new method is added.

--- my-reddit-backend/src/main/java/com/company/myredditbackend/persistence/repository/VoteRepository.java
+++ my-reddit-backend/src/main/java/com/company/myredditbackend/persistence/repository/VoteRepository.java
@@ -1,7 +1,12 @@
 package com.company.myredditbackend.persistence.repository;
 
+import com.company.myredditbackend.persistence.model.Post;
+import com.company.myredditbackend.persistence.model.User;
 import com.company.myredditbackend.persistence.model.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface VoteRepository extends JpaRepository<Vote, Long> {
+    Optional<Vote> findTopByPostAndUserOrderByIdDesc(Post post, User currentUser);
 }
