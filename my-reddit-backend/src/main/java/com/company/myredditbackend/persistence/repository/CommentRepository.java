e5fd9121fb1ff2eac47b0d4df488469ea694989a
Serife Erguner <seriferabia@gmail.com>
2020-07-12 18:12:42 +0200

    New methods are added.

--- my-reddit-backend/src/main/java/com/company/myredditbackend/persistence/repository/CommentRepository.java
+++ my-reddit-backend/src/main/java/com/company/myredditbackend/persistence/repository/CommentRepository.java
@@ -1,7 +1,14 @@
 package com.company.myredditbackend.persistence.repository;
 
 import com.company.myredditbackend.persistence.model.Comment;
+import com.company.myredditbackend.persistence.model.Post;
+import com.company.myredditbackend.persistence.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByPost(Post post);
+
+    List<Comment> findAllByUser(User user);
 }
