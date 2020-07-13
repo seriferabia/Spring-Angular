6c33d34ee4ff159e70a565d4510d7755b0aa3d04
Serife Erguner <seriferabia@gmail.com>
2020-07-13 17:19:18 +0200

    Refresh token request dto is created and new methods are added into repository.

--- my-reddit-backend/src/main/java/com/company/myredditbackend/persistence/repository/RefreshTokenRepository.java
+++ my-reddit-backend/src/main/java/com/company/myredditbackend/persistence/repository/RefreshTokenRepository.java
@@ -4,5 +4,10 @@ package com.company.myredditbackend.persistence.repository;
 import com.company.myredditbackend.persistence.model.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+
+    void deleteByToken(String token);
 }
