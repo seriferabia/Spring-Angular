acb58f7eb87b37f4b58c5f308eeb9fa3f10e10e5
Serife Erguner <seriferabia@gmail.com>
2020-07-14 13:18:54 +0200

    SwaggerConfiguration is imported.

--- my-reddit-backend/src/main/java/com/company/myredditbackend/MyRedditBackendApplication.java
+++ my-reddit-backend/src/main/java/com/company/myredditbackend/MyRedditBackendApplication.java
@@ -1,11 +1,14 @@
 package com.company.myredditbackend;
 
+import com.company.myredditbackend.configuration.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@Import(SwaggerConfiguration.class)
 public class MyRedditBackendApplication {
 
     public static void main(String[] args) {
