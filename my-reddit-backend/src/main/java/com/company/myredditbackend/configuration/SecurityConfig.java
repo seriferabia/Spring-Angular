fc47bf8baf0b7de42183f61c07ece1861b646dcf
Serife Erguner <seriferabia@gmail.com>
2020-08-07 17:39:43 +0200

    SecurityConfig is improved.

--- my-reddit-backend/src/main/java/com/company/myredditbackend/configuration/SecurityConfig.java
+++ my-reddit-backend/src/main/java/com/company/myredditbackend/configuration/SecurityConfig.java
@@ -30,7 +30,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf().disable()
+        httpSecurity.cors().and()
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/api/auth/**")
                 .permitAll()
