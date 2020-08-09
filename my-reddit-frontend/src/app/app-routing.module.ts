1c431391533001d6a9f9f0eb0e9562c0f37e2363
Serife Erguner <seriferabia@gmail.com>
2020-08-09 16:02:58 +0200

    To protect routes 'CanActivate' guard is added.

--- my-reddit-frontend/src/app/app-routing.module.ts
+++ my-reddit-frontend/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {SignupComponent} from './auth/signup/signup.component';
 import {LoginComponent} from './auth/login/login.component';
 import {HomeComponent} from './home/home.component';
@@ -8,22 +8,23 @@ import {CreateSubredditComponent} from './subreddit/create-subreddit/create-subr
 import {ListSubredditsComponent} from './subreddit/list-subreddits/list-subreddits.component';
 import {ViewPostComponent} from './post/view-post/view-post.component';
 import {UserProfileComponent} from './auth/user-profile/user-profile.component';
-
+import {AuthGuard} from './auth/auth.guard';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'sign-up',  component: SignupComponent},
+  {path: 'sign-up', component: SignupComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'create-post', component: CreatePostComponent},
-  {path: 'create-subreddit', component: CreateSubredditComponent},
+  {path: 'create-post', component: CreatePostComponent, canActivate: [AuthGuard]},
+  {path: 'create-subreddit', component: CreateSubredditComponent, canActivate: [AuthGuard]},
   {path: 'list-subreddits', component: ListSubredditsComponent},
   {path: 'view-post/:id', component: ViewPostComponent},
-  {path: 'user-profile/:username', component: UserProfileComponent}
+  {path: 'user-profile/:username', component: UserProfileComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
